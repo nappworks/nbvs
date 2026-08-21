4a61af9cb24069973b7706b07797be9771bf93af
nao.n <131720899+nappworks@users.noreply.github.com>
2026-08-22 07:24:57 +0900

    position一致判定APIを公開

--- src/nbvs.nim
+++ src/nbvs.nim
@@ -8,12 +8,13 @@
 ## * `nbvs/elias_fano`
 ## * `nbvs/wavelet_matrix`
 ## * `nbvs/reversed_wavelet_matrix`
+## * `nbvs/wavelet_position_match`
 ##
 ## Import this module when you want the complete `nbvs` API.
 
 import nbvs/[bit_vector, packed_array, succinct_bit_vector, elias_fano,
-  wavelet_matrix, reversed_wavelet_matrix, run_length_bwt, succinct_radix_trie,
-  fm_dictionary]
+  wavelet_matrix, reversed_wavelet_matrix, wavelet_position_match,
+  run_length_bwt, succinct_radix_trie, fm_dictionary]
 
 export bit_vector
 export packed_array
@@ -21,6 +22,7 @@ export succinct_bit_vector
 export elias_fano
 export wavelet_matrix
 export reversed_wavelet_matrix
+export wavelet_position_match
 export run_length_bwt
 export succinct_radix_trie
 export fm_dictionary
