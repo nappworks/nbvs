97966c2cb06edc74774d94808f0431c3d8df00e6
nao.n <131720899+nappworks@users.noreply.github.com>
2026-09-05 06:36:20 +0900

    Export Wavelet matching run API

--- src/nbvs.nim
+++ src/nbvs.nim
@@ -10,12 +10,14 @@
 ## * `nbvs/reversed_wavelet_matrix`
 ## * `nbvs/wavelet_position_match`
 ## * `nbvs/wavelet_select_cursor`
+## * `nbvs/wavelet_matching_runs`
 ##
 ## Import this module when you want the complete `nbvs` API.
 
 import nbvs/[bit_vector, packed_array, succinct_bit_vector, elias_fano,
   wavelet_matrix, reversed_wavelet_matrix, wavelet_position_match,
-  wavelet_select_cursor, run_length_bwt, succinct_radix_trie, fm_dictionary]
+  wavelet_select_cursor, wavelet_matching_runs, run_length_bwt,
+  succinct_radix_trie, fm_dictionary]
 
 export bit_vector
 export packed_array
@@ -25,6 +27,7 @@ export wavelet_matrix
 export reversed_wavelet_matrix
 export wavelet_position_match
 export wavelet_select_cursor
+export wavelet_matching_runs
 export run_length_bwt
 export succinct_radix_trie
 export fm_dictionary
