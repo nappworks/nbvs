3c24ff1e36d8615840e03cd01323468327be0eef
nao.n <131720899+nappworks@users.noreply.github.com>
2026-09-05 09:35:41 +0900

    Test cursor-backed matching runs

--- tests/twavelet_matching_runs.nim
+++ tests/twavelet_matching_runs.nim
@@ -51,10 +51,12 @@ block simpleRuns:
     (left: 5'i64, right: 7'i64)]
 
   doAssert wm.matchingRuns(7) == expectedSevens
+  doAssert wm.matchingRunsCursor(7) == expectedSevens
   doAssert wm.collectMatchingRuns(7) == expectedSevens
   doAssert wm.matchingRuns(1) == @[
     (left: 0'i64, right: 1'i64),
     (left: 4'i64, right: 5'i64)]
+  doAssert wm.matchingRunsCursor(1) == wm.matchingRuns(1)
   doAssert wm.matchingRuns(7, 2, 6) == @[
     (left: 2'i64, right: 4'i64),
     (left: 5'i64, right: 6'i64)]
@@ -64,17 +66,26 @@ block simpleRuns:
     iterated.add run
   doAssert iterated == expectedSevens
 
+  var cursorIterated: seq[MatchingRun]
+  for run in wm.matchingRunsCursorItems(7):
+    cursorIterated.add run
+  doAssert cursorIterated == expectedSevens
+
 block edgeCases:
   let empty = genWaveletMatrix(newSeq[uint64]())
   doAssert empty.matchingRuns(0).len == 0
+  doAssert empty.matchingRunsCursor(0).len == 0
 
   let zeros = genWaveletMatrix(@[0'u64, 0, 0, 0])
   doAssert zeros.matchingRuns(0) == @[
     (left: 0'i64, right: 4'i64)]
+  doAssert zeros.matchingRunsCursor(0) == zeros.matchingRuns(0)
   doAssert zeros.matchingRuns(1).len == 0
+  doAssert zeros.matchingRunsCursor(1).len == 0
 
   let wm = genWaveletMatrix(@[1'u64, 2, 3])
   doAssert wm.matchingRuns(100).len == 0
+  doAssert wm.matchingRunsCursor(100).len == 0
   doAssert wm.matchingRuns(2, 1, 1).len == 0
   expectRaises(IndexDefect): discard wm.matchingRuns(2, -1, 2)
   expectRaises(IndexDefect): discard wm.matchingRuns(2, 0, 4)
@@ -84,6 +95,7 @@ block prefixSplits:
   let xs = @[7'u64, 6, 7, 7, 4, 7, 6, 7]
   let wm = genWaveletMatrix(xs, 3)
   doAssert wm.matchingRuns(7) == naiveRuns(xs, 7, 0, xs.len)
+  doAssert wm.matchingRunsCursor(7) == wm.matchingRuns(7)
 
 block wordBoundaries:
   var xs = newSeq[uint64](140)
@@ -94,6 +106,7 @@ block wordBoundaries:
       xs[i] = uint64(i mod 5)
   let wm = genWaveletMatrix(xs)
   doAssert wm.matchingRuns(7) == naiveRuns(xs, 7, 0, xs.len)
+  doAssert wm.matchingRunsCursor(7) == wm.matchingRuns(7)
 
 block randomRuns:
   var rng = initRand(0x52554e53)
@@ -112,4 +125,6 @@ block randomRuns:
         naiveRuns(xs, value, left, right)
 
     for value in 0'u64..20'u64:
-      doAssert wm.matchingRuns(value) == naiveRuns(xs, value, 0, xs.len)
+      let expected = naiveRuns(xs, value, 0, xs.len)
+      doAssert wm.matchingRuns(value) == expected
+      doAssert wm.matchingRunsCursor(value) == expected
