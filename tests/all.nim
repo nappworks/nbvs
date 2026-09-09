d33c0da241fe28d31ab1ae7c7d07e523ddbb7019
nao.n <131720899+nappworks@users.noreply.github.com>
2026-09-09 20:53:16 +0900

    Include QuadVector tests in test suite

--- tests/all.nim
+++ tests/all.nim
@@ -5,6 +5,7 @@ import ./tpacked_array
 import ./tpacked_array_view
 import ./tstructure_views
 import ./tsuccinct_bit_vector
+import ./tquad_vector
 import ./tbit_vector_select_cursor
 import ./telias_fano
 import ./twavelet_matrix
