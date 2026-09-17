12e820afc7c0c756617b0ab72476609bff185fed
nao.n <131720899+nappworks@users.noreply.github.com>
2026-09-17 18:59:13 +0900

    feat: export Wavelet Matrix terminal APIs

--- src/nbvs.nim
+++ src/nbvs.nim
@@ -12,6 +12,7 @@
 ## * `nbvs/bit_vector_select_cursor`
 ## * `nbvs/elias_fano`
 ## * `nbvs/wavelet_matrix`
+## * `nbvs/wavelet_terminal`
 ## * `nbvs/reversed_wavelet_matrix`
 ## * `nbvs/wavelet_position_match`
 ## * `nbvs/wavelet_select_cursor`
@@ -22,9 +23,9 @@
 import nbvs/[bit_vector, packed_array, succinct_bit_vector, quad_vector,
   quad_vector_view, quad_wavelet_matrix, hybrid_wavelet_matrix_9,
   bit_vector_select_cursor, elias_fano,
-  wavelet_matrix, reversed_wavelet_matrix, wavelet_position_match,
-  wavelet_select_cursor, wavelet_matching_runs, run_length_bwt,
-  succinct_radix_trie, fm_dictionary]
+  wavelet_matrix, wavelet_terminal, reversed_wavelet_matrix,
+  wavelet_position_match, wavelet_select_cursor, wavelet_matching_runs,
+  run_length_bwt, succinct_radix_trie, fm_dictionary]
 
 export bit_vector
 export packed_array
@@ -36,6 +37,7 @@ export hybrid_wavelet_matrix_9
 export bit_vector_select_cursor
 export elias_fano
 export wavelet_matrix
+export wavelet_terminal
 export reversed_wavelet_matrix
 export wavelet_position_match
 export wavelet_select_cursor
